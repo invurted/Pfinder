50aee34c09574dcb2c8a4e59a00f4f6ce0bcdb00
Adam Jones <two1274@hotmail.com>
2012-09-04 07:44:27 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Finder::Application.config.secret_token = 'cae5bdb96b3d43643e2cb563b2d7e6bf05215561c52f618e9d30917871387781b6e846fd2f9548da6989b9f5b0fe424364b0151d16316a6977372bf8e6a702c7'
